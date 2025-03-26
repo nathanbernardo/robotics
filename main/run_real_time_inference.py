b9f27e4f954f176fa769ff9033990f415da6ce28
Nathan <ncbernar@uci.edu>
2025-03-25 21:01:57 -0700

    new change

--- main/run_real_time_inference.py
+++ main/run_real_time_inference.py
@@ -2,7 +2,7 @@
 import freenect
 import numpy as np
 import cv2 as cv
-from ultralytics import YOLO
+from ultralytics import YOLO, solutions
 from freenect import DEPTH_MM
 from kinect_utils.frame_convert import video_cv
 from rich.console import Console
@@ -10,11 +10,17 @@ from rich.panel import Panel
 from rich.table import Table
 
 console = Console()
+distancecalculator = solutions.DistanceCalculation(
+    model="/home/ncbernar/.pyenv/runs/obb/train58/weights/best.pt",  # path to the YOLO11 model file.
+    show=True,  # display the output
+)
 
 class KinectProcessor:
-    def __init__(self, model_path, calibration_file):
-        self.model = YOLO(model_path)
-        self.labels = self.model.names
+    def __init__(self, detection_model, obb_model, calibration_file):
+        self.detection_model = YOLO(detection_model)
+        self.obb_model = YOLO(obb_model)
+        self.detection_labels = self.detection_model.names
+        self.obb_labels = self.obb_model.names
         self.load_calibration(calibration_file)
 
     def load_calibration(self, calibration_file):
@@ -46,6 +52,7 @@ class KinectProcessor:
         h, w = frame.shape[:2]
         newCameraMtx, roi = cv.getOptimalNewCameraMatrix(self.mtx, self.dist, (w, h), 1, (w, h))
 
+
         dst = cv.undistort(frame, self.mtx, self.dist, None, newCameraMtx)
 
         # crop the image
@@ -64,24 +71,102 @@ class KinectProcessor:
         real_z = depth
 
         return real_x, real_y, real_z
-
+    
+    # def process_detections(self, results, labels, frame, table):
+    #     for result in detection_results[0].boxes:
+    #         class_index = result.cls[0].item()
+    #         class_name = self.detection_labels[class_index]
+    #         x1, y1, x2, y2 = result.xyxy[0]
+    #
+    #         # Calculate center point
+    #         center_x = int((x1 + x2) / 2)
+    #         center_y = int((y1 + y2) / 2)
+    #
+    #         # Get distance based on center points
+    #         distance = self.get_center_depth(center_x, center_y)
+    #
+    #         # Draw center point on the frame
+    #         cv.circle(detection_frame, (center_x, center_y), 5, (0, 255, 0), -1)
+    #
+    #         if distance is not None:
+    #             real_x, real_y, real_z = self.get_real_world_coordinates(center_x, center_y, distance)
+    #             
+    #             table.add_row(
+    #                 f"{class_name.capitalize()}",
+    #                 f"({center_x}, {center_y})",
+    #                 f"{distance}mm",
+    #                 f"X: {real_x:.2f}mm, Y: {real_y:.2f}mm, Z: {real_z:.2f}mm"
+    #             )
+    #
+    #         else:
+    #             table.add_row(
+    #                 f"Object {len(table.rows) + 1}",
+    #                 f"({center_x}, {center_y})",
+    #                 "N/A",
+    #                 "N/A"
+    #             )
 
     def process_frame(self, frame):
         undistorted_frame = self.undistort_frame(frame)
-        results = self.model(undistorted_frame)
-        annoted_frame = results[0].plot()
 
+        
+        # Run both models
+        detection_results = self.detection_model(undistorted_frame)
+        obb_results = self.obb_model(undistorted_frame)
 
-        table = Table(title="Object Detection Results")
-        table.add_column("Object", style="cyan")
-        table.add_column("Center", style="magenta")
-        table.add_column("Distance", style="green")
-        table.add_column("Real-world Coordinates", style="yellow")
+        # annoted_frame = results[0].plot()
 
-        for result in results[0].boxes:
+        # Create combined annotations
+        obb_frame = obb_results[0].plot()
+        detection_frame = detection_results[0].plot()
+        # combined_frame = cv.addWeighted(obb_frame, 0.9, detection_frame, 0.9, 0)
+        combined_frame = obb_results[0].plot()
+        # combined_frame = detection_results[0].plot(img=combined_frame)
+
+        # table = Table(title="Object Detection Results")
+        # table.add_column("Object", style="cyan")
+        # table.add_column("Center", style="magenta")
+        # table.add_column("Distance", style="green")
+        # table.add_column("Real-world Coordinates", style="yellow")
+
+        # print("RESULTS: ", results[0].obb)
+        # for result in detection_results[0].boxes:
+        #     class_index = result.cls[0].item()
+        #     class_name = self.detection_labels[class_index]
+        #     x1, y1, x2, y2 = result.xyxy[0]
+        #
+        #     # Calculate center point
+        #     center_x = int((x1 + x2) / 2)
+        #     center_y = int((y1 + y2) / 2)
+        #
+        #     # Get distance based on center points
+        #     distance = self.get_center_depth(center_x, center_y)
+        #
+        #     # Draw center point on the frame
+        #     cv.circle(detection_frame, (center_x, center_y), 20, (0, 255, 0), -1)
+        #
+        #     if distance is not None:
+        #         real_x, real_y, real_z = self.get_real_world_coordinates(center_x, center_y, distance)
+        #         
+        #         table.add_row(
+        #             f"{class_name.capitalize()}",
+        #             f"({center_x}, {center_y})",
+        #             f"{distance}mm",
+        #             f"X: {real_x:.2f}mm, Y: {real_y:.2f}mm, Z: {real_z:.2f}mm"
+        #         )
+        #
+        #     else:
+        #         table.add_row(
+        #             f"Object {len(table.rows) + 1}",
+        #             f"({center_x}, {center_y})",
+        #             "N/A",
+        #             "N/A"
+        #         )
+
+        for result in obb_results[0].obb:
             class_index = result.cls[0].item()
-            class_name = self.labels[class_index]
-            x1, y1, x2, y2 = result.xyxy[0]
+            class_name = self.obb_labels[class_index]
+            x1, y1, x2, y2, _ = result.xywhr[0]
 
             # Calculate center point
             center_x = int((x1 + x2) / 2)
@@ -90,39 +175,46 @@ class KinectProcessor:
             # Get distance based on center points
             distance = self.get_center_depth(center_x, center_y)
 
+
             # Draw center point on the frame
-            cv.circle(annoted_frame, (center_x, center_y), 5, (0, 255, 0), -1)
+            # cv.circle(combined_frame, (center_x, center_y), 20, (0, 255, 0), -1)
 
-            if distance is not None:
-                real_x, real_y, real_z = self.get_real_world_coordinates(center_x, center_y, distance)
-                
-                table.add_row(
-                    f"{class_name.capitalize()}",
-                    f"({center_x}, {center_y})",
-                    f"{distance}mm",
-                    f"X: {real_x:.2f}mm, Y: {real_y:.2f}mm, Z: {real_z:.2f}mm"
-                )
-
-            else:
-                table.add_row(
-                    f"Object {len(table.rows) + 1}",
-                    f"({center_x}, {center_y})",
-                    "N/A",
-                    "N/A"
-                )
-        console.print(table)
-        return annoted_frame
+        #     if distance is not None:
+        #         real_x, real_y, real_z = self.get_real_world_coordinates(center_x, center_y, distance)
+        #         
+        #         table.add_row(
+        #             f"{class_name.capitalize()}",
+        #             f"({center_x}, {center_y})",
+        #             f"{distance}mm",
+        #             f"X: {real_x:.2f}mm, Y: {real_y:.2f}mm, Z: {real_z:.2f}mm"
+        #         )
+        #
+        #     else:
+        #         table.add_row(
+        #             f"Object {len(table.rows) + 1}",
+        #             f"({center_x}, {center_y})",
+        #             "N/A",
+        #             "N/A"
+        #         )
+        # console.print(table)
+        return combined_frame
 
 def main():
-    model_path = "/home/ncbernar/.pyenv/runs/detect/train9/weights/best.pt"
-    calibraiton_file = "../utils/kinect/camera_calibration.npz"
-    processor = KinectProcessor(model_path, calibraiton_file)
+    MODEL_PATHS = {
+        'detection': "/home/ncbernar/.pyenv/runs/detect/train9/weights/best.pt",
+        'obb': "/home/ncbernar/.pyenv/runs/obb/train58/weights/best.pt"
+    }
+    CALIBRATION_FILE = "../utils/kinect/camera_calibration.npz"
+    processor = KinectProcessor(MODEL_PATHS['detection'], MODEL_PATHS['obb'], CALIBRATION_FILE)
 
+    # Initialize distance calculation object
 
     # with 
     while True:
         frame = processor.get_video()
         annotated_frame = processor.process_frame(frame)
+        results = distancecalculator.calculate(annotated_frame)
+        print("RESULTS: ", results)
 
         cv.imshow("YOLOv11 Inference", annotated_frame)
 
--- utils/object_detection/organize_dir.py
+++ utils/object_detection/organize_dir.py
@@ -1,4 +1,3 @@
-
 import os
 import shutil
 from pathlib import Path
@@ -6,42 +5,53 @@ import yaml
 import argparse
 import glob
 from rich.progress import track
+from rich.console import Console
 
-parser = argparse.ArgumentParser(description="Program to organizing dataset directory for Ultralytics")
-parser.add_argument("--path", type=Path, default=Path('.'), help="Path to the target directory (default: current directory)")
-parser.add_argument("--train", type=Path, required=True, help="Path to the training dataset")
-parser.add_argument("--val", type=Path, required=True, help="Path to the validation dataset")
-parser.add_argument("--train_images", type=Path, required=True, help="Path to images for training dataset")
-parser.add_argument("--validation_images", type=Path, required=True, help="Path to images for validation dataset")
-args = parser.parse_args()
+console = Console()
 
-base_path = args.path.absolute()
-train_path = args.train.absolute()
-train_images_path = args.train_images.absolute()
-val_path = args.val.absolute()
-val_images_path = args.validation_images.absolute()
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Program to organizing dataset directory for Ultralytics")
+    parser.add_argument("--path", type=Path, default=Path('.'), help="Path to the target directory (default: current directory)")
+    parser.add_argument("--train", type=Path, required=True, help="Path to the training dataset")
+    parser.add_argument("--val", type=Path, required=True, help="Path to the validation dataset")
+    parser.add_argument("--train_images", type=Path, required=True, help="Path to images for training dataset")
+    parser.add_argument("--validation_images", type=Path, required=True, help="Path to images for validation dataset")
+
+    return parser.parse_args()
+
 
 # Create directories for storing image dataset
-for folder in ['train', 'val']:
-    for subdir in ['images', 'labels']:
-        (base_path / folder / subdir).mkdir(parents=True, exist_ok=True)
+def create_directories(base_path):
+    console.log("Creating directories...")
+    for folder in ['train', 'val']:
+        for subdir in ['images', 'labels']:
+            dir_path = base_path / folder / subdir
+            dir_path.mkdir(parents=True, exist_ok=True)
+            console.log(f"Created directory: [bold green]{dir_path}")
+            # (base_path / folder / subdir).mkdir(parents=True, exist_ok=True)
 
 def merge_txt_files(train_file, validation_file, output_file):
+    console.log(f"Merging text files into [bold]{output_file}[/bold]...")
     with open(output_file, 'w') as outfile:
         for fname in [train_file, validation_file]:
             with open(fname, 'r') as infile:
                 outfile.write(infile.read())
 
 def copy_labels(src_dir, dst_dir):
+    console.log(f"Copying labels from [bold]{src_dir}[/bold] to [bold]{dst_dir}[/bold]...")
     for file in os.listdir(src_dir):
         if file.endswith(".txt"):
             shutil.copy(os.path.join(src_dir, file), dst_dir)
+    console.log("[bold green]Labels copied successfully![/bold]")
 
 def copy_images(src_path, dst_dir):
+    console.log(f"Copying images from [bold]{src_path}[/bold] to [bold]{dst_dir}[/bold]...")
     for img in glob.glob(os.path.join(src_path, "*.png")):
         shutil.copy(img, dst_dir)
+    console.log("[bold green]Images copied successfully![/bold]")
 
 def update_yaml(src_yaml, dst_yaml):
+    console.log(f"Updating YAML file: [bold]{dst_yaml}[/bold]...")
     with open(src_yaml, 'r') as file:
         yaml_data = yaml.safe_load(file)
 
@@ -54,34 +64,49 @@ def update_yaml(src_yaml, dst_yaml):
     with open(dst_yaml, 'w') as file:
         yaml.dump(yaml_data, file, default_flow_style=False)
 
+    console.log("[bold green]YAML file successfully updated")
+
 def removed_unlabeled_images(src_dir, label_path):
+    console.log(f"Removing unlabeled images from [bold]{src_dir}[/bold]...")
     labeled_images = set(f"{os.path.splitext(label)[0]}.png" for label in os.listdir(label_path) if label.endswith('.txt'))
 
     # Remove unlabeled images
-    for img in track(os.listdir(src_dir)):
+    for img in track(os.listdir(src_dir), description="Processing images..."):
         if img not in labeled_images:
             os.remove(f"{src_dir}/{img}")
+    
+    console.log("[bold green]Unlabeled images removed successfully![/bold]")
 
-print("Merging text files...")
-merge_txt_files(f"{train_path}/Train.txt", f"{val_path}/Validation.txt", f"{base_path}/Data.txt")
-print("Successfully merged text files into Data.txt")
 
-print("Copying labels to new directory")
-copy_labels(f"{train_path}/labels/Train", f"{base_path}/train/labels")
-copy_labels(f"{val_path}/labels/Validation", f"{base_path}/val/labels")
-print("Successfully copied labels to new directory")
+def main():
+    args = parse_arguments()
 
-print("Copying images to new directory")
-copy_images(train_images_path, f"{base_path}/train/images")
-copy_images(val_images_path, f"{base_path}/val/images")
-print("Successfully copied images to new directory")
+    base_path = args.path.absolute()
+    train_path = args.train.absolute()
+    train_images_path = args.train_images.absolute()
+    val_path = args.val.absolute()
+    val_images_path = args.validation_images.absolute()
 
-print("Removing unlabeled images...")
-removed_unlabeled_images(f"{base_path}/train/images", f"{train_path}/labels/Train")
-removed_unlabeled_images(f"{base_path}/val/images", f"{val_path}/labels/Validation")
-print("Successfully removed unlabeled images")
+    # Step 1: Create necessary directories for storing images and labels
+    create_directories(base_path)
 
-print("Updating data.yaml...")
-update_yaml(f"{train_path}/data.yaml", f"{base_path}/data.yaml")
-print("Successfully updated yaml")
+    # Step 2: Merge training and validation text files
+    merge_txt_files(f"{train_path}/Train.txt", f"{val_path}/Validation.txt", f"{base_path}/Data.txt")
 
+    # Step 3: Copy label files to new directories
+    copy_labels(f"{train_path}/labels/Train", f"{base_path}/train/labels")
+    copy_labels(f"{val_path}/labels/Validation", f"{base_path}/val/labels")
+
+    # Step 4: Copy image files to new directories
+    copy_images(train_images_path, f"{base_path}/train/images")
+    copy_images(val_images_path, f"{base_path}/val/images")
+
+    # Step 5: Removed unlabeled images
+    removed_unlabeled_images(f"{base_path}/train/images", f"{train_path}/labels/Train")
+    removed_unlabeled_images(f"{base_path}/val/images", f"{val_path}/labels/Validation")
+
+    # Step 6: Update YAML configuration file
+    update_yaml(f"{train_path}/data.yaml", f"{base_path}/data.yaml")
+
+if __name__ == "__main__":
+    main()
