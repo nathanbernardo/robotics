699a3ab63b8d05d46715f6132fb8b6e6718a87d9
Nathan <ncbernar@uci.edu>
2025-03-26 23:00:05 -0700

    feat: Adding chungus urdf

---
+++ kinectmatics/ik.py
@@ -0,0 +1,15 @@
+import ikpy.chain
+import numpy as np
+import ikpy.utils.plot as plot_utils
+import matplotlib.pyplot as plt
+
+my_chain = ikpy.chain.Chain.from_urdf_file('./poppy_ergo.URDF')
+
+target_position = [ 0.1, -0.2, 0.1]
+print("The angles of each joints are: ", my_chain.inverse_kinematics(target_position))
+
+fig, ax = plot_utils.init_3d_figure()
+my_chain.plot(my_chain.inverse_kinematics(target_position, 'x'), ax, target=target_position)
+plt.xlim(-0.1, 0.1)
+plt.ylim(-0.1, 0.1)
+plt.show()
