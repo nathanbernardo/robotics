96af128ab3226053dc626e25a7937e8532e24377
Myron Phan <myronphan@Myrons-MBP.attlocal.net>
2025-05-28 21:45:14 -0700

    updates for testing

--- packages/arduino/main.ino
+++ packages/arduino/main.ino
@@ -1,3 +1,4 @@
+// base motor
 #define X_STEP_PIN         54
 #define X_DIR_PIN          55
 #define X_ENABLE_PIN       38
@@ -10,20 +11,30 @@
 #define Y_MIN_PIN          14
 #define Y_MAX_PIN          15
 
+// pitch motor
 #define Z_STEP_PIN         46
 #define Z_DIR_PIN          48
 #define Z_ENABLE_PIN       62
 #define Z_MIN_PIN          18
 #define Z_MAX_PIN          19
 
+// yaw motor
 #define E0_STEP_PIN        26
 #define E0_DIR_PIN         28
 #define E0_ENABLE_PIN      24
 
+// roll motor
+#define E1_STEP_PIN        36
+#define E1_DIR_PIN         34
+#define E1_ENABLE_PIN      30
+
+// shoulder motor
 #define DM556T_PULSE       16
 #define DM556T_DIR         17
-#define DM542_PULSE        23
-#define DM542_DIR          25
+
+// third motor
+#define DM542_DIR          23
+#define DM542_PULSE        25
 #define SERVO_DRIVER_SCC   27
 #define SERVO_DRIVER_SDA   29
 
@@ -49,10 +60,12 @@ float actuatorPosition5;
 float actuatorPosition6;
 
 // Define the stepper motor and the pins that is connected to
-AccelStepper stepper1(1, X_STEP_PIN, X_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
-AccelStepper stepper2(1, E0_STEP_PIN, E0_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
-AccelStepper stepper3(1, Y_STEP_PIN, Y_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
-AccelStepper baseStepper(1, Z_STEP_PIN, Z_DIR_PIN);
+AccelStepper baseStepper(1, X_STEP_PIN, X_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
+AccelStepper yawStepper(1, E0_STEP_PIN, E0_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
+AccelStepper rollStepper(1, E1_STEP_PIN, E1_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
+AccelStepper pitchStepper(1, Z_STEP_PIN, Z_DIR_PIN);
+AccelStepper shoulderStepper(1, DM556T_PULSE, DM556T_DIR);
+AccelStepper thirdStepper(AccelStepper::DRIVER, DM542_PULSE, DM542_DIR);
 
 void setup() {
   Serial.begin(9600);
@@ -60,23 +73,33 @@ void setup() {
 
   pinMode(X_ENABLE_PIN    , OUTPUT);
   pinMode(E0_ENABLE_PIN    , OUTPUT);
+  pinMode(E1_ENABLE_PIN    , OUTPUT);
   pinMode(Z_ENABLE_PIN    , OUTPUT);
   // Set maximum speed value for the stepper
-  stepper1.setMaxSpeed(200);
-  stepper1.setAcceleration(400);
-  stepper1.setCurrentPosition(0);
-
-  stepper2.setMaxSpeed(200);
-  stepper2.setAcceleration(200);
-  stepper2.setCurrentPosition(0);
-
-  stepper3.setMaxSpeed(200);
-  stepper3.setAcceleration(200);
-  stepper3.setCurrentPosition(0);
-
   baseStepper.setMaxSpeed(200);
-  baseStepper.setAcceleration(200);
+  baseStepper.setAcceleration(400);
   baseStepper.setCurrentPosition(0);
+
+  yawStepper.setMaxSpeed(200);
+  yawStepper.setAcceleration(200);
+  yawStepper.setCurrentPosition(0);
+
+  rollStepper.setMaxSpeed(200);
+  rollStepper.setAcceleration(200);
+  rollStepper.setCurrentPosition(0);
+
+  shoulderStepper.setMaxSpeed(300);
+  shoulderStepper.setAcceleration(300);
+  shoulderStepper.setCurrentPosition(0);
+
+  pitchStepper.setMaxSpeed(100);
+  pitchStepper.setAcceleration(100);
+  pitchStepper.setCurrentPosition(0);
+
+  thirdStepper.setMaxSpeed(100);
+  thirdStepper.setAcceleration(100);
+  thirdStepper.setCurrentPosition(0);
+  
 
 }
 
@@ -106,44 +129,82 @@ void loop() {
       Serial.flush();
       if (iter == 0)
       {
-        stepper1.move(uh);
-        while(abs(stepper1.distanceToGo()) > 0)
+        baseStepper.move(uh);
+        while(abs(baseStepper.distanceToGo()) > 0)
         {
           if (uh < 0)
           {
-            stepper1.setMaxSpeed(-400);
+            baseStepper.setMaxSpeed(-400);
           }
           else
           {
-            stepper1.setMaxSpeed(400);
+            baseStepper.setMaxSpeed(400);
           }
-          stepper1.run();
+          baseStepper.run();
         }
       }
       if (iter == 1)
       {
-        stepper2.move(uh);
-        while(abs(stepper2.distanceToGo()) > 0)
+        shoulderStepper.move(uh);
+        while(abs(shoulderStepper.distanceToGo()) > 0)
         {
           if (uh < 0)
-            stepper2.setSpeed(-200);
+            shoulderStepper.setSpeed(-300);
           else
-            stepper2.setSpeed(200);
-            stepper2.run();
+            shoulderStepper.setSpeed(300);
+          shoulderStepper.run();
           
         }
       }
 
       if (iter == 2)
       {
-        baseStepper.move(uh);
-        while(abs(baseStepper.distanceToGo()) > 0)
+        thirdStepper.move(uh);
+        while(abs(thirdStepper.distanceToGo()) > 0)
         {
           if (uh < 0)
-            baseStepper.setSpeed(-200);
+            thirdStepper.setSpeed(-100);
           else
-            baseStepper.setSpeed(200);
-          baseStepper.run();
+            thirdStepper.setSpeed(100);
+          thirdStepper.run();
+        }
+      }
+
+      if (iter == 3)
+      {
+        pitchStepper.move(uh);
+        while(abs(pitchStepper.distanceToGo()) > 0)
+        {
+          if (uh < 0)
+            pitchStepper.setSpeed(-100);
+          else
+            pitchStepper.setSpeed(100);
+          pitchStepper.run();
+        }
+      }
+
+      if (iter == 4)
+      {
+        yawStepper.move(uh);
+        while(abs(yawStepper.distanceToGo()) > 0)
+        {
+          if (uh < 0)
+            yawStepper.setSpeed(-100);
+          else
+            yawStepper.setSpeed(100);
+          yawStepper.run();
+        }
+      }
+      if (iter == 5)
+      {
+        rollStepper.move(uh);
+        while(abs(rollStepper.distanceToGo()) > 0)
+        {
+          if (uh < 0)
+            rollStepper.setSpeed(-100);
+          else
+            rollStepper.setSpeed(100);
+          rollStepper.run();
         }
       }
 
